30a6b01f4b01e126f28858edcc21caef761d7d89
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-22 13:47:39 -0700

    stuff

--- example-004/main.tf
+++ example-004/main.tf
@@ -1,13 +1,19 @@
 terraform {
   required_providers {
     incus = {
-      source  = "lxc/incus"
+      source = "lxc/incus"
     }
   }
 }
 
-resource "incus_image" "example_image" {
+resource "incus_image" "example_image1" {
   source_remote = "images"
   source_image  = "alpine/3.16"
   aliases       = ["example-003"]
 }
+
+# resource "incus_image" "example_image2" {
+#   source_remote = "local"
+#   source_image  = "example-003"
+#   aliases       = ["example-003-test"]
+# }
--- src/theremay/examples.py
+++ src/theremay/examples.py
@@ -51,4 +51,20 @@ examples = [
         {"other_stuff": True},
         {"templates": []},
     ],
+    [
+        {"other_stuff": True},
+        {
+            "templates": [
+                {"template": "README.md.j2", "outfile": "README.md"},
+                {"template": "run.sh.j2", "outfile": "run.sh"},
+                {"template": "clean.sh.j2", "outfile": "clean.sh"},
+                {
+                    "template": "ubuntu.pkr.hcl.j2",
+                    "outfile": "ubuntu.pkr.hcl",
+                },
+                {"template": "main.tf.j2", "outfile": "main.tf"},
+                {"template": "provision.sh.j2", "outfile": "provision.sh"},
+            ]
+        },
+    ],
 ]
